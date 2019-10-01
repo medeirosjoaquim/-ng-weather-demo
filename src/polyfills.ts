c2fd136b752f67e0bfa709f419308325581fd503
Joaquim Medeiros <joao.junior@br.experian.com>
2019-09-30 22:54:10 -0300

    add nextTick

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -58,6 +58,9 @@
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 (window as any).process = {
   env: { DEBUG: undefined },
+  nextTick() {
+    return null;
+  }
 };
 
 (window as any).global = {
