70cbfc0e5bc726a24779841d45ca3448cb47ffc7
Joaquim Medeiros <joao.junior@br.experian.com>
2019-09-30 23:04:38 -0300

    fix algolia places for angular

--- src/app/weather/search-place/search-place.component.ts
+++ src/app/weather/search-place/search-place.component.ts
@@ -1,26 +1,23 @@
-import { Component, AfterViewInit, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import * as places from 'places.js';
 @Component({
   selector: 'app-search-place',
   templateUrl: './search-place.component.html',
   styleUrls: ['./search-place.component.scss']
 })
-export class SearchPlaceComponent implements OnInit {
+export class SearchPlaceComponent implements AfterViewInit {
   @ViewChild('addressInput', {static: true }) private addressInput: ElementRef;
 
 
   constructor() { }
 
-  ngOnInit() {
-    console.log(this.addressInput);
-  }
-  ngAfterViewInit() {
+   ngAfterViewInit() {
     const placesAutocomplete = places({
       appId: 'plYLJA6WH2HP',
       apiKey: 'fb7e3a103ac9a2e341b5ce364c05998e',
       container: this.addressInput.nativeElement
     });
+    placesAutocomplete.on('change', e => console.log(e.suggestion));
 
   }
-
 }
