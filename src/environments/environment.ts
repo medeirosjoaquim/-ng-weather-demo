c32d940b2b5469423fc33ae2d0f5dd44fc33fb0e
Joaquim Medeiros <joao.junior@br.experian.com>
2019-10-03 12:33:27 -0300

    fix api url for prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,16 +6,13 @@ export const environment = {
   production: false,
   openWeather: {
     openWeatherKEy: `ca8ac29b2be0f1c6cf2c5ae6d3cc5826`,
-    openWeatherApiUrl: `http://api.openweathermap.org/data/2.5/forecast?&lang=en&units=metric`,
-    openWeatherDailyApiUrl: `http://api.openweathermap.org/data/2.5/forecast/daily?&lang=en&units=metric`,
+    openWeatherApiUrl: `api.openweathermap.org/data/2.5/forecast?&lang=en&units=metric`,
+    openWeatherDailyApiUrl: `api.openweathermap.org/data/2.5/forecast/daily?&lang=en&units=metric`,
   },
   algolia: {
     algoliaPlacesId: `plYLJA6WH2HP`,
     algoliaPlacesKey: `fb7e3a103ac9a2e341b5ce364c05998e`,
   }
-
-
-
 };
 
 /*
